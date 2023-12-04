b534757b1e302fcc566ba10c10e170a739d9ea65
Moysés Rocha <152832684+mozarocha@users.noreply.github.com>
2023-12-04 00:39:29 -0300

    Opção de Saída

--- Cartorio.cpp
+++ Cartorio.cpp
@@ -131,6 +131,7 @@ int main () //fun
 		printf("\t1 -  Registrar nomes\n");
 		printf("\t2 -  Consultar nomes\n");
 		printf("\t3 -  Deletar nomes\n\n");
+		printf("\t4 -  Sair do sistema\n\n");
 		printf("Opção:  "); //Fim do menu
 				
 		scanf("%d", &opcao); //Armazenando a escolha do usuário
@@ -150,6 +151,11 @@ int main () //fun
 			
 			case 3:
 			deletar();
+			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistemaa!\n");
+			return 0;
 			break;
 			
 			default:
